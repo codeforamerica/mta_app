b6402c5790ad56da3ff937b9c9bd6620a7ad463a
Justin R. Miller <incanus@codesorcery.net>
2012-03-27 16:13:58 -0700

    Initial Commit

---
+++ MapBox Example/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MapBox Example
+//
+//  Created by Justin Miller on 3/27/12.
+//  Copyright (c) 2012 MapBox / Development Seed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
